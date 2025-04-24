52744b0556e56efe824aed0642d1846c1694764e
wolf-domination <165001309+wolf-domination@users.noreply.github.com>
2025-04-23 19:27:22 -0500

    Update index.js

--- backend/db/models/index.js
+++ backend/db/models/index.js
@@ -1,3 +1,58 @@
+
+
+
+
+'use strict';
+const fs = require('fs');
+const path = require('path');
+const Sequelize = require('sequelize');
+const process = require('process');
+const basename = path.basename(__filename);
+const env = process.env.NODE_ENV || 'development';
+const config = require(__dirname + '/../../config/database.js')[env];
+const db = {};
+let sequelize;
+
+// This handles Render's DATABASE_URL environment variable
+if (process.env.DATABASE_URL) {
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  });
+} else if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
+
+fs.readdirSync(__dirname)
+  .filter(file => file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js' && file.indexOf('.test.js') === -1)
+  .forEach(file => {
+    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+    db[model.name] = model;
+  });
+
+Object.keys(db).forEach(modelName => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
+
+
+
+
+
+
 // 'use strict';
 
 // const fs = require('fs');
@@ -54,6 +109,8 @@
 
 //Trouble shoot code.
 // ✅ Ensure Sequelize instance is properly initialized
+
+/*
 'use strict';
 
 const fs = require('fs');
@@ -95,7 +152,7 @@ console.log("Sequelize initialized with config:", config); // Debugging output
 
 module.exports = db;
 
-
+*/
 
 
 
