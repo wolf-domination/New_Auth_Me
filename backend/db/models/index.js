b68a89e18cd59cf448a6dc4f54f433329a4f6446
wolf-domination <165001309+wolf-domination@users.noreply.github.com>
2025-04-23 19:37:33 -0500

    Update index.js

--- backend/db/models/index.js
+++ backend/db/models/index.js
@@ -13,6 +13,67 @@ const config = require(__dirname + '/../../config/database.js')[env];
 const db = {};
 let sequelize;
 
+// Debug output to see what's available
+console.log("Environment:", env);
+console.log("Database URL exists:", !!process.env.DATABASE_URL);
+
+if (env === 'production') {
+  // Direct approach for production environment
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    },
+    define: {
+      schema: process.env.SCHEMA
+    }
+  });
+} else if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
+
+fs.readdirSync(__dirname)
+  .filter(file => file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js' && file.indexOf('.test.js') === -1)
+  .forEach(file => {
+    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+    db[model.name] = model;
+  });
+
+Object.keys(db).forEach(modelName => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
+
+
+
+
+
+
+
+
+/*
+'use strict';
+const fs = require('fs');
+const path = require('path');
+const Sequelize = require('sequelize');
+const process = require('process');
+const basename = path.basename(__filename);
+const env = process.env.NODE_ENV || 'development';
+const config = require(__dirname + '/../../config/database.js')[env];
+const db = {};
+let sequelize;
+
 // This handles Render's DATABASE_URL environment variable
 if (process.env.DATABASE_URL) {
   sequelize = new Sequelize(process.env.DATABASE_URL, {
@@ -47,7 +108,7 @@ db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
 module.exports = db;
-
+*/
 
 
 
