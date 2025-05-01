28ed9ef2856b5f296ec9a655f268ecae97879820
wolf-domination <165001309+wolf-domination@users.noreply.github.com>
2025-05-01 10:21:55 -0500

    Update index.js

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -16,7 +16,7 @@ const apiRouter = require('./api');
 // backend/routes/index.js
 // ...
 // Add a XSRF-TOKEN cookie
-router.get("/api/csrf/restore", (req, res) => {
+router.get("/api/csrf/restore/get", (req, res) => {
     const csrfToken = req.csrfToken();
     res.cookie("XSRF-TOKEN", csrfToken);
     res.status(200).json({
