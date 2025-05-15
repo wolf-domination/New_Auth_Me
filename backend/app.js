d4da676e3e9aa27c3346cf08493ab0d1e789ea0a
wolf-domination <165001309+wolf-domination@users.noreply.github.com>
2025-05-15 03:13:06 -0500

    05/15-update

--- backend/app.js
+++ backend/app.js
@@ -51,7 +51,11 @@ if (!isProduction) {
   // );
 
 
-
+app.use(cors({
+  origin:'http:localhost:5173',
+  methods:['GET','POST','PUT','DELETE'],
+  allowedHeaders:['Content-Type','XSRF-TOKEN']
+}));
 
 
 app.use(routes); // Connect all the routes
